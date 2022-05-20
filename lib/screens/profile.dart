784b90ba12aa58c7898d4e797efbfe58fd4ddbe7
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2022-05-20 22:01:13 +0500

    profile screen

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/order_provider.dart';
 
 class Profile extends StatelessWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Profile'),
+    final order = Provider.of<OrderProvider>(context);
+    return Column(
+      children: [
+        Container(
+          height: 150,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(15),
+              bottomRight: Radius.circular(15),
+            ),
+          ),
+          child: Text('Cart'),
+        ),
+        Container(
+          child: Text('Orders'),
+        ),
+      ],
     );
   }
 }
