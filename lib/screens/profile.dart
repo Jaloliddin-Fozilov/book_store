5657ce81310f0ef895d54f919a6713175404ea18
Jaloliddin-Fozilov <jaloliddinish@gmail.com>
2022-05-24 20:43:00 +0500

    islogin

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../widgets/profile_follower.dart';
+import '../screens/profile_page_screen.dart';
+import '../screens/sign_screen.dart';
 
 import '../providers/author_provider.dart';
 
@@ -10,87 +11,9 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isLogin = false;
+
     final author = Provider.of<AuthorProvider>(context).list[0];
-    return Column(
-      children: [
-        Stack(
-          clipBehavior: Clip.none,
-          alignment: AlignmentDirectional.center,
-          children: [
-            Container(
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(100),
-                  bottomRight: Radius.circular(100),
-                ),
-              ),
-            ),
-            Positioned(
-              top: 30,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: Image.network(
-                  author.imageUrl,
-                  width: 120,
-                  height: 120,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          ],
-        ),
-        Container(
-          margin: const EdgeInsets.only(top: 60, left: 60, right: 60),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ListTile(
-                leading: const Icon(
-                  Icons.person,
-                ),
-                title: Text(author.name),
-              ),
-              ListTile(
-                leading: const Icon(
-                  Icons.mail,
-                ),
-                title: Text(author.email),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ProfileFollower(
-                    text: 'Followers',
-                    number: author.followers,
-                  ),
-                  ProfileFollower(
-                    text: 'Following',
-                    number: author.following,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  ElevatedButton(
-                    onPressed: () =>
-                        Navigator.of(context).pushNamed('/edit-profile'),
-                    child: const Text('Edit Profile'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () =>
-                        Navigator.of(context).pushNamed('/dashboard'),
-                    child: const Text('Dashboard'),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
+    return isLogin ? ProfileScreen(authorId: author.id) : SignScreen();
   }
 }
---
+++ lib/screens/profile_page_screen.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/author_provider.dart';
+
+import '../widgets/profile_follower.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({
+    Key? key,
+    required this.authorId,
+  }) : super(key: key);
+  final String authorId;
+  @override
+  Widget build(BuildContext context) {
+    final author = Provider.of<AuthorProvider>(context).findById(authorId);
+    return Column(
+      children: [
+        Stack(
+          clipBehavior: Clip.none,
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              height: 100,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColor,
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(100),
+                  bottomRight: Radius.circular(100),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 30,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: Image.network(
+                  author.imageUrl,
+                  width: 120,
+                  height: 120,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 60, left: 60, right: 60),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ListTile(
+                leading: const Icon(
+                  Icons.person,
+                ),
+                title: Text(author.name),
+              ),
+              ListTile(
+                leading: const Icon(
+                  Icons.mail,
+                ),
+                title: Text(author.email),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ProfileFollower(
+                    text: 'Followers',
+                    number: author.followers,
+                  ),
+                  ProfileFollower(
+                    text: 'Following',
+                    number: author.following,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () =>
+                          Navigator.of(context).pushNamed('/edit-profile'),
+                      child: const Text('Edit Profile'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () =>
+                          Navigator.of(context).pushNamed('/dashboard'),
+                      child: const Text('Dashboard'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
